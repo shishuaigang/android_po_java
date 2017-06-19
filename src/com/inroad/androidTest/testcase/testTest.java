0dae6ff054f8532dd7521a4f4034b5982b533486
shishuaigang <shishuaigang@163.com>
2017-06-19 11:50:35 +0800

    添加testng 测试用的测试用例

---
+++ src/com/inroad/androidTest/testcase/testTest.java
@@ -0,0 +1,50 @@
+package com.inroad.androidTest.testcase;
+
+import com.inroad.androidTest.common.Flag;
+import com.inroad.androidTest.common.Login;
+import com.inroad.androidTest.driver.Driver;
+import com.inroad.androidTest.page.Home;
+import com.inroad.androidTest.page.Knowledge;
+import com.inroad.androidTest.page.Menu;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static com.inroad.androidTest.page.Home.switchbtn;
+import static com.inroad.androidTest.page.Knowledge.browse;
+import static com.inroad.androidTest.page.Menu.knowledge;
+
+/**
+ * Created by shishuaigang on 2017/6/19.
+ */
+public class testTest {
+
+    private AndroidDriver driver;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new Driver().get_driver();
+        int i;
+        i = new Flag(driver).pagejudge();
+        Login ll = new Login("18121225109", "123456", driver);
+        ll.login_all(i);
+        Home p1 = PageFactory.initElements(driver, Home.class);
+        Menu p2 = PageFactory.initElements(driver, Menu.class);
+        Knowledge p3 = PageFactory.initElements(driver, Knowledge.class);
+    }
+
+    @Test
+    public void TestNgLearn() {
+        switchbtn.click();
+        knowledge.click();
+        browse.click();
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+}
