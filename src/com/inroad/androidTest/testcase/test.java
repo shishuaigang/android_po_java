0f98eb6bae6d6e61a59756c7a73d13cde5697908
shishuaigang <shishuaigang@163.com>
2017-06-19 10:09:55 +0800

    尝试使用pagefactory

--- src/com/inroad/androidTest/testcase/test.java
+++ src/com/inroad/androidTest/testcase/test.java
@@ -4,9 +4,13 @@ import com.inroad.androidTest.common.*;
 import com.inroad.androidTest.driver.Driver;
 import com.inroad.androidTest.page.*;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import static com.inroad.androidTest.page.TestPage.*;
 
 /**
  * Created by shishuaigang on 2017/6/15.
+ * 测试用testcase
  */
 public class test {
     public static void main(String args[]) {
@@ -15,8 +19,10 @@ public class test {
         i = new Flag(d).pagejudge();
         Login ll = new Login("18121225109", "123456", d);
         ll.login_all(i);
-        d.findElement(Home.switch_btn).click();
-        d.findElement(Menu.knowledge).click();
-        d.findElement(Knowledge.browse).click();
+        TestPage testPage = PageFactory.initElements(d,TestPage.class);
+        switchbtn.click();
+        //d.findElement(Home.switch_btn).click();
+        //d.findElement(Menu.knowledge).click();
+        //d.findElement(Knowledge.browse).click();
     }
 }
